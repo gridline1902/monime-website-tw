8baa96ec42af97e859ccaf163512b8caa34813fc
gridline1902 <michaeljrfreeman@gmail.com>
2024-11-15 18:14:31 +0000

    updates.....

--- src/app/_components/IntegrationStats.tsx
+++ src/app/_components/IntegrationStats.tsx
@@ -8,7 +8,7 @@ export default function IntegrationStats() {
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-4xl px-6 lg:px-8">
-        <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+        <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center items-center md:grid-cols-3 lg:grid-cols-3">
           {stats.map((stat) => (
             <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
               <dt className="text-base/7 text-gray-600">{stat.name}</dt>
