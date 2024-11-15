2051cedb73e7122332ed51c18547e890521610d8
gridline1902 <michaeljrfreeman@gmail.com>
2024-11-15 16:34:51 +0000

    update.....

--- src/app/_components/PartnersSection.tsx
+++ src/app/_components/PartnersSection.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import OrangeImg from "@/images/Orange-2.png";
@@ -8,7 +8,6 @@ import GTBankImg from '@/images/GTBank-2.png';
 import QMoneyImg from '@/images/QMoney-2.png';
 import clsx from "clsx";
 
-
 const imagePaths = [
     { index: 0, src: OrangeImg, alt: 'Orange' },
     { index: 1, src: AfrimoneyImg, alt: 'Afrimoney' },
@@ -17,7 +16,6 @@ const imagePaths = [
     { index: 4, src: QMoneyImg, alt: 'QMoney' }
 ];
 
-
 export default function TrustedPartners() {
     return (
         <div className="max-w-6xl mx-auto space-y-8">
@@ -25,22 +23,24 @@ export default function TrustedPartners() {
                 <p className="font-medium">Trusted by these partners</p>
             </div>
             <div className="max-w-5xl mx-auto">
-                <div className="flex flex-col sm:flex-row items-center gap-8 sm:gap-20">
+                <div className="grid grid-cols-2 mx-auto place-items-center md:flex md:flex-row md:gap-20 items-center gap-8">
                     {imagePaths.map((image) => (
                         <div
                             key={image.index}
-                            className="relative w-full sm:w-[400px] h-[70px] overflow-hidden"
+                            className={clsx(`${image.index === 4 && "relative scale-150"} relative w-[200px] h-[90px] overflow-hidden`)}
                         >
                             <Image
                                 src={image.src}
                                 alt={image.alt}
                                 fill
-                                className={clsx(`${image.index === 4 ? 'object-fill scale-50' : 'object-contain'} hover:transform hover:transition-transform hover:ease-in-out hover:-translate-y-4 hover:duration-300`)}
+                                className={clsx(
+                                    `${image.index === 4 ? 'object-contain scale-50 bg-red-500 w-full' : 'object-cover'} hover:transform hover:transition-transform hover:ease-in-out hover:-translate-y-4 hover:duration-300`
+                                )}
                             />
                         </div>
                     ))}
                 </div>
             </div>
         </div>
-    )
+    );
 }
